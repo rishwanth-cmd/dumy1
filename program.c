86963e4b0b59eb66f28ed31714aa194e6c43c40e
rishwanthk <iamrishwanthk@gmail.com>
2025-10-10 08:45:27 +0530

    first commit

--- program.c
+++ program.c
@@ -1,157 +1,230 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
-#include <stdlib.h>
 
-double evaluate_helper(double a, double b, char op);
-double evaluate_expression(const char* expression);
-int precedence_checker(char op);
+typedef struct {
+    int id;
+    char name[50];
+    int age;
+} User;
 
+// Function prototypes
+void createUser();
+void readUser();
+void updateUser();
+void deleteUser();
 
 int main()
 {
-    char expression[100] = "";            
-    while(strlen(expression)==0)
+    bool turn = true;
+    while (turn)
     {
-        printf("Enter the expression: ");
-        fgets(expression, sizeof(expression), stdin);    
-        expression[strlen(expression)-1]='\0';   
-
-    }
-    double result = evaluate_expression(expression);   
-    if(result==-1)
-    {
-        printf("Please enter a valid expression.");
-    }
-    else
-    {
-        printf("%.2lf",result);
-    }
-    return 0;
-
-}
-
-double evaluate_expression(const char* expression)
-{
-    double operands[100];   
-    char operators[100];   
-    int i=0, len=strlen(expression);
-    int operands_top=-1, operators_top=-1;
-
-    while(i<len)
-    {
-        char ch = expression[i];
-
-        if(ch==' ')
+        printf("1. Create User\n");
+        printf("2. Read Users\n");
+        printf("3. Update User\n");
+        printf("4. Delete User\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        int choice;
+        //validating user input
+        if(scanf("%d", &choice)!=1)
         {
-            i++;
+            printf("Please enter a valid option.\n");
+            while(getchar() != '\n');
             continue;
         }
-        
-        if(ch>='0' && ch<='9')
+
+
+        switch (choice)
         {
-            char numArr[50]="";
-            int j=0;
-            while(i<len && ((expression[i]>='0' && expression[i]<='9') || expression[i]=='.'))
-            {
-                numArr[j++] = expression[i++];
-            }
-            numArr[j]='\0';
-            double num = atof(numArr);
-            operands[++operands_top] = num;
+            case 1:
+                createUser();
+                break;
+            case 2:
+                readUser();
+                break;
+            case 3:
+                updateUser();
+                break;
+            case 4:
+                deleteUser();
+                break;
+            case 5:
+                turn = false;
+                break;
+            default:
+                printf("Invalid choice. Please choose a valid option.\n");
         }
-        else if(ch=='+' || ch=='-' || ch=='*' || ch=='/')
+    }
+
+    return 0;
+}
+
+void createUser()
+{
+    User user;
+
+    FILE *file = fopen("users.txt", "r");
+    if (!file)
+    {
+        file = fopen("users.txt", "w");
+        fclose(file);
+        file = fopen("users.txt", "r");
+    }
+
+    printf("Enter User ID: \n");
+    scanf("%d",&user.id);
+    getchar();
+
+    //checking for duplicate id's
+    User temp;
+    int found=0;
+    while(fscanf(file, "User ID: %d, User Name: %49[^,], User Age: %d\n", &temp.id, temp.name, &temp.age) == 3)
+    {
+        if(temp.id==user.id)
         {
-            if(operands_top<0)
-            {
-                printf("Oops! The expression you have entered is invalid.\n");
-                return -1;
+            found=1;
+            break;
+        }
+    }
+    fclose(file);
+    if(found)
+    {
+        printf("User ID: %d already exists. Please enter a new ID.\n",user.id);
+        return;
+    }
 
-            }
-            while(operators_top>=0 && precedence_checker(operators[operators_top])>=precedence_checker(ch))
-            {
-                if(operands_top<1)
-                {
-                    printf("Oops! The expression you have entered is invalid.\n");
-                    return -1;
-                }
-                double b = operands[operands_top--];
-                double a = operands[operands_top--];
-                char op = operators[operators_top--];
-                double res = evaluate_helper(a,b,op);
+    printf("Enter User Name: \n");
+    fgets(user.name, sizeof(user.name), stdin);
+    user.name[strlen(user.name)-1]='\0';
 
-                if(res==-1)
-                {
-                    return -1;
-                }
+    printf("Enter User Age: ");
+    scanf("%d",&user.age);
 
-                operands[++operands_top] = res;
-            }
+    file = fopen("users.txt", "a");
 
-            operators[++operators_top] = ch;
-            i++;
+
+    fprintf(file, "User ID: %d, User Name: %s, User Age: %d\n",user.id,user.name,user.age);
+    fclose(file);
+
+    printf("User added successfully\n");
+}
+
+void readUser()
+{
+    FILE *file = fopen("users.txt","r");
+
+    if(file==NULL)
+    {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    int id;
+    char name[50];
+    int age;
+
+    while(fscanf(file, "User ID: %d, User Name: %49[^,], User Age: %d\n", &id, name, &age)==3)
+    {
+        printf("User ID: %d, User Name: %s, User Age: %d\n",id,name,age);
+    }
+
+    fclose(file);
+}
+
+
+void updateUser()
+{
+    User user;
+    int id;
+    char newName[50];
+    int newAge;
+    int found=0;
+    printf("Enter the ID to update: ");
+    scanf("%d",&id);
+    getchar();
+
+    FILE *file = fopen("users.txt","r");
+    FILE *temp = fopen("newFile.txt","w");
+    if(file==NULL || temp==NULL)
+    {
+        printf("Error opening file.\n");
+        return;
+    }
+    while(fscanf(file, "User ID: %d, User Name: %49[^,], User Age: %d\n", &user.id, user.name, &user.age)==3)
+    {
+        if(id==user.id)
+        {
+            printf("Enter new User Name: ");
+            fgets(newName, sizeof(newName), stdin);
+            newName[strlen(newName)-1]='\0';
+
+            printf("Enter new User Age: ");
+            scanf("%d",&newAge);
+
+            fprintf(temp, "User ID: %d, User Name: %s, User Age: %d\n", user.id, newName, newAge);
+            found=1;
         }
         else
         {
-            printf("Oops! The expression you have entered is invalid.\n");
-            return -1;
+            fprintf(temp, "User ID: %d, User Name: %s, User Age: %d\n", user.id, user.name, user.age);
+
         }
     }
-
-    while(operators_top>=0)
+    fclose(file);
+    fclose(temp);
+    remove("users.txt");
+    rename("newFile.txt","users.txt");
+    if(found)
     {
-         if(operands_top<1)
-         {
-             printf("Oops! The expression you have entered is invalid.\n");
-             return -1;
-         }
-        double b = operands[operands_top--];
-        double a = operands[operands_top--];
-        char op = operators[operators_top--];
-        double res = evaluate_helper(a, b, op);
+        printf("User ID: %d updated successfully.\n",id);
+    }
+    else
+    {
+        printf("No user found with User ID: %d\n",id);
+    }
 
-        if(res==-1)
+}
+
+
+void deleteUser()
+{
+    User user;
+    int id;
+    printf("Enter the User ID to delete: ");
+    scanf("%d",&id);
+    int found=0;
+
+    FILE *file = fopen("users.txt","r");
+    FILE *temp = fopen("newFile.txt","w");
+
+    if(file==NULL || temp==NULL)
+    {
+        printf("Error opening in file.\n");
+        return;
+    }
+
+    while(fscanf(file, "User ID: %d, User Name: %49[^,], User Age: %d\n", &user.id, user.name, &user.age)==3)
+    {
+        if(user.id==id)
         {
-            return -1;
+            found=1;
+            continue;
         }
-
-        operands[++operands_top] = res;
+        fprintf(temp, "User ID: %d, User Name: %s, User Age: %d\n", user.id, user.name, user.age);
     }
+    fclose(file);
+    fclose(temp);
 
-    return operands[operands_top];
-}
+    remove("users.txt");
+    rename("newFile.txt", "users.txt");
 
-int precedence_checker(char op)
-{
-    if(op == '/' || op == '*')
+    if(found==0)
     {
-        return 2;
+        printf("No user was found with the ID: %d\n",id);
     }
-    if(op == '+' || op == '-')
+    else
     {
-        return 1;
+        printf("User ID: %d was deleted successfully.\n",id);
     }
-    return 0;
-}
-
-double evaluate_helper(double a, double b, char op)
-{
-    switch (op)
-    {
-        case '+':
-            return a+b;
-        case '-':
-            return a-b;
-        case '*':
-            return a*b;
-        case '/':
-            if(b==0)
-            {
-                printf("Cannot divide by zero.\n");
-                return -1;
-            }
-            return a/b;
-        default:
-            break;
-    }
-    return 0;
 }
